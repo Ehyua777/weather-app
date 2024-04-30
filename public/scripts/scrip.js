05da07f3379c95ac168ecc5969867631ada71995
Ehyua777 <successful.ehyua@outlook.com>
2024-04-30 07:00:38 +0000

    dernière mise à jour du 29/04/24

--- public/scripts/scrip.js
+++ public/scripts/scrip.js
@@ -35,14 +35,14 @@ async function main(withIP = true) {
     if (withIP) {
         // 1) recuperer l'adresse IP du terminal qui accède à la page avec l'API:
         // https://api.ipify.org?format=json
-        ip = await fetch('https://api.ipify.org?format=json')
+        ip = await fetch('http://api.ipify.org?format=json')
             .then(result => result.json())
             .then(json => json.ip)
-        //console.log(ip)
+        console.log(ip)
 
         // 2) recuperer la ville de l'utilisateur à partir avec l'API:
         // https://freegeoip.net/json+ip
-        ville = await fetch(`https://ip-api.com/json/${ip}`)
+        ville = await fetch(`http://ip-api.com/json/${ip}`)
             .then(result => result.json())
             .then(json => json.city)
         //console.log(ville)
@@ -53,14 +53,12 @@ async function main(withIP = true) {
 
     // 3) Récupération de la météo à partir fde l'API 
     // http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric
-    const meteo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric`).then(result => result.json()).then(json => json)
+    const meteo = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=8e602b9ea28ed4f9f8fc97a5f6d1105c&lang=fr&units=metric`).then(result => result.json()).then(json => json)
     //console.log(meteo);
 
     //4) Aficher les information sur la page
     displayWeatherInfos(meteo);
 }
-
-
 
 const ville = document.querySelector('#ville');
 
